492c6a20efeb42b8e98b3e1994a2c7cda070ba6f
jawlo6 <januszwlodarz@gmail.com>
2024-03-28 16:41:57 +0100

    back.html blokada cofinięcia przeglądarki

--- egzam.php
+++ egzam.php
@@ -43,7 +43,7 @@
     //losuj pytania
     
     $sql="SELECT * from PYTANIA where zestaw=\"". $zestaw."\""; 
- //   echo $sql;
+    echo $sql;
     $con = mysqli_connect("localhost","root","","egzamin");
 // echo $sql;
     $result = mysqli_query($con,$sql);
